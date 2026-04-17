7ed0eee53528066659fb049f5da02dd768101a65
Moises Alejandro <moisesalejandro052@gmail.com>
2026-04-17 00:42:07 -0400

    Termine de hacer un ejemplo de 5

--- usoArchivos5/archivos.php
+++ usoArchivos5/archivos.php
@@ -11,10 +11,21 @@
         $tam=$_FILES['file1']['size'];
         echo $tam."<br>";
         $extension=pathinfo($_FILES['file1']['name'],PATHINFO_EXTENSION);
-        echo $extension;
-        if ($extension=="pdf") {
+        echo $extension."<br>";
+        if (($extension=="pdf"||$extension=="docx")) {
             # code...
-            echo "Holaaaaa";
+            if ($_FILES['file1']['size']<=2000) {
+                # code...
+                echo "<h3>archivo valido</h3><br>";
+                echo "$nombre - $tam";
+            }
+            else{
+                //echo "<h3>tamano superior a los 2 mb</h3>"; 
+                echo "$tam excedido<br>";   
+            }
+        }
+        else{
+            echo "$extension extension no permitida<br>";
         }
     }
     else{
